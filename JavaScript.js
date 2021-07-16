6d3e634fc1fbb1282a7c9f2ea631ff2a2249fb8c
BringsCoding <simonbrings@googlemail.com>
2021-07-16 08:52:05 +0200

    commit kat

--- JavaScript.js
+++ JavaScript.js
@@ -7,19 +7,44 @@ let checkbox = document.createElement("input");
 
 button.addEventListener("click", function () {
   let ul = document.querySelector("#myUL"); //ul
-  let li = document.createElement("li"); //li
-  let p = document.createElement("p");
+  let lia = document.querySelector("#li-element");
+  //let li = document.createElement("li"); //li
+  let paragraph = document.createElement("p");
+  let pa = document.querySelector("p");
 
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.value = 1;
   checkbox.id = "checkboxen";
+  checkbox.className = "check";
 
-  li.appendChild(checkbox);
+  //paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = textfild.value;
+  textfild.value = "";
+  //li.appendChild(document.createTextNode(text.value));
+  lia.appendChild(checkbox);
+  lia.appendChild(paragraph);
 
-  let text = document.querySelector("#textfild");
-  p.innerText = text.value;
-  li.appendChild(document.createTextNode(text.value));
-
-  ul.appendChild(li);
+  /////////////////////////////////////////////////////
+  checkbox.addEventListener("change", function (event) {
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    let pa = checkbox.parentNode.parentNode;
+    if (checked) {
+      paragraph.id = "responded";
+    } else {
+      paragraph.id = "";
+    }
+  });
 });
+
+let list = document.getElementsByClassName("responded");
+for (var i = 0; i < list.length; i++) {
+  list[i].parentNode.removeChild(list[i]);
+  console.log(list);
+}
+
+//let text = document.querySelector("#textfild");
+//li.appendChild(document.createTextNode(text.value));
+//li.appendChild(document.createTextNode(textfild.value));
+//ul.appendChild(li);
