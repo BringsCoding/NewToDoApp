b35a990a0af108c755555af1f111fa3e18738722
BringsCoding <simonbrings@googlemail.com>
2021-09-07 00:07:17 +0200

    fix:button delete

--- JavaScript.js
+++ JavaScript.js
@@ -1,57 +1,59 @@
-console.log("hello");
-
-const arr = [];
+let arr = [];
 
 const newlist = document.querySelector("#myUL");
 const addbutton = document.querySelector("#addme-button");
 const textfield = document.querySelector("#textfild");
+AddButton();
+function AddButton() {
+  addbutton.addEventListener("click", function () {
+    const newtext = textfield.value;
+    let newarray = { description: newtext, done: false };
 
-addbutton.addEventListener("click", function () {
-  const newtext = textfield.value;
-  const newarray = { description: newtext, done: false };
-  arr.push(newarray);
-  textfield.value = "";
-  ////JavasScript ^^^^^^^^////////
+    arr.push(newarray);
+    textfield.value = "";
+    ////JavasScript ^^^^^^^^////////
+    //sendMe();
+    //Bedigung damit mindestens 5 buschataben geschrieben werden
+    if (newtext.length < 5) {
+      alert("Ihr Text ist zu kurz, mindestens 5 Buschtaben");
+      return;
+    }
 
-  //Bedigung damit mindestens 5 buschataben geschrieben werden
-  if (newtext.length < 5) {
-    alert("Ihr Text ist zu kurz, mindestens 5 Buschtaben");
-    return;
-  }
+    const newli = document.createElement("li");
+    newli.todoObj = newarray;
+    const textNode = document.createTextNode(newtext);
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.value = 1;
+    checkbox.id = "checkboxen";
+    checkbox.className = "check";
+    let label = document.createElement("label");
+    const todoid = newtext.trim().toLowerCase().replaceAll(" ", "-"); //Wandeln die ID um
 
-  const newli = document.createElement("li");
-  newli.todoObj = newarray;
-  const textNode = document.createTextNode(newtext);
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.value = 1;
-  checkbox.id = "checkboxen";
-  checkbox.className = "check";
-  let label = document.createElement("label");
-  const todoid = newtext.trim().toLowerCase().replaceAll(" ", "-"); //Wandeln die ID um
+    newli.appendChild(checkbox);
+    newli.appendChild(label);
+    label.append(textNode);
+    newlist.appendChild(newli);
+    label.setAttribute("for", todoid);
 
-  newli.appendChild(checkbox);
-  newli.appendChild(label);
-  label.append(textNode);
-  newlist.appendChild(newli);
-  label.setAttribute("for", todoid);
+    //JSON //////
+    JSON.stringify(newarray);
+    localStorage.setItem("list", JSON.stringify(newarray));
+    //localStorage.getItem("list");
 
-  //JSON //////
-  JSON.stringify(arr);
-  localStorage.setItem("list", JSON.stringify(arr));
-  localStorage.getItem("list");
-  JSON.parse(localStorage.getItem("list"));
-  success();
+    if (localStorage.getItem("list")) {
+      newarray = JSON.parse(localStorage.getItem("list"));
+    } else {
+      newarray = [];
+    }
 
-  //Funktionen für den Filter
-  ButtonAll();
-  ButtonOpen();
-  ButtonDone();
+    //Funktionen für den Filter
+    ButtonAll();
+    ButtonOpen();
+    ButtonDone();
+  });
+}
 
-  readColorsFromLocalStorage();
-});
-
-//Überprüft die Checboxen
 newlist.addEventListener("change", function (e) {
   const newDoneState = e.target.checked;
   const todoObj = e.target.parentElement.todoObj;
@@ -103,12 +105,6 @@ function ButtonDone() {
     }
   });
 }
-//const allopen = document.querySelector("#button-open");
-//allopen.addEventListener("click", function () {
-//for (let i = 0; i < newlist.children.length; i++) {
-//newlist.children[i].hidden = false;
-//}
-//});
 
 const remove = document.querySelector("#remove-button");
 remove.addEventListener("click", function () {
@@ -125,4 +121,15 @@ function success() {
   }
 }
 
-// initially read colors from local storage
+//Fetch//TEST
+
+test();
+function test() {
+  const ButtonFetch = document.querySelector("#fetch");
+  ButtonFetch.addEventListener("click", function () {
+    fetch("http://localhost:4730/todos/69", {
+      method: "DELETE",
+    });
+  });
+}
+/////////////////////////////////
